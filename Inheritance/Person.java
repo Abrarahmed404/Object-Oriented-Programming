ec3d9d0730a14c2467a576032acd63b99ffded0d
Abrarahmed404 <unarabrar80@gmail.com>
2026-06-01 09:15:41 +0500

    Added a banking system created using abstract classes and interfaces

---
+++ Inheritance/Person.java
@@ -0,0 +1,12 @@
+public class Person{
+  String name;
+  int age;
+  Person(String name, int age){
+    this.name = name;
+    this.age = age;
+  }
+  
+  void display(){
+    System.out.println("Name ="+this.name+"\n Age = "+this.age);
+  }
+}
