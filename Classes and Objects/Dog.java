c9a82e73588ef498eab7785f62b922f48c283a36
Abrarahmed404 <unarabrar80@gmail.com>
2026-06-06 12:46:15 +0500

    Implemented a Dog class

---
+++ Classes and Objects/Dog.java
@@ -0,0 +1,24 @@
+class Dog{   
+    String name;
+    static int legs=4;
+    static int tail=1;
+    String color;
+
+
+Dog(String name,String color){
+
+    this.name=name;
+    this.color=color;
+}
+
+
+
+    void eat(){
+        System.out.println("eating.....");
+    }
+
+    void barks(){
+        System.out.println("bhow bhow bhow");
+    }
+
+}
