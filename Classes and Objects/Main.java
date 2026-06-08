2947a5d1b528cad5ccf995e6bf5214c1b5a37382
Abrarahmed404 <unarabrar80@gmail.com>
2026-06-08 23:09:27 +0500

    Updated Main.java

--- Classes and Objects/Main.java
+++ Classes and Objects/Main.java
@@ -1,29 +1,38 @@
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] ahj){
 
-        Dog d1 = new Dog("Tommy", "Brown"); //A dog obj is created 
+        //A dog obj is created 
+        Dog d1 = new Dog("Tommy", "Brown"); 
 
-        d1.barks();                                    //Barks methode is called
+        //Barks methode is called
+        d1.barks();                                    
+        
 
+        //Car object using default constructor
+        Car c1 = new Car();   
+        //It has not any values given yet 
+        
+        c1.name = "Toyota";
+        c1.model = "Unknown";
+        c1.year = 2024;
+        c1.Engine ="V8 Engine";
+        //car object is complete
 
-Car c1 = new Car();    //Car object using default constructor
-                       //It has not any values given yet 
-c1.name = "Toyota";
-c1.model = "Unknown";
-c1.year = 2024;
-c1.Engine ="V8 Engine";
-                       //car object is complete
+        //Car object using parameterized constructor
+        Car c2 = new Car("Maruti","Unknown",2025,"V6 Engine");
+        //Here values are given when object is bieng created and constructor is called.
+        
+        
+        c2. carInfo();
+        System.out.println();
 
-Car c2 = new Car("Maruti","Unknown",2025,"V6 Engine");
-                        //Car object using parameterized constructor
-                        //Here values are given when object is bieng created and constructor is called.
+        
+        Book b1 = new Book("Intersteller", "Erwin shrewdinger",22084, "7th");
+        
+        Book b2 = new Book("Lamed", "Abrar Ahmed",22076, "1st");
 
-
-c2. carInfo();
-
-
-
-
-
+        b1.displayBook();
+        System.out.println();
+        b2.displayBook();
     }
 }
