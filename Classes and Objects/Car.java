ea51d808daf477b8c61998265cd15b01991ecb8e
Abrarahmed404 <unarabrar80@gmail.com>
2026-06-08 23:12:59 +0500

    Updated Car.java

--- Classes and Objects/Car.java
+++ Classes and Objects/Car.java
@@ -5,10 +5,12 @@ public class Car {
     int year;
     String Engine;
 
-                      //A constructor which will initialize the properties of objects
+    //A constructor which will initialize the properties of objects
     Car(String name, String model,int year,String Engine){
+
+        //this keyword will diffrentiate b/w instance variables(having this key word) and parameter variables.
         this.name=name;
-        this.model=model;
+        this.model=model;  
         this.year=year;
         this.Engine=Engine;
     }
@@ -17,7 +19,7 @@ public class Car {
         
     }
 
-                //The car will start if you insert key (parameterized Method)
+    //The car will start if you insert key (parameterized Method)
     public void start(int key){
         if (key==1) {
             System.out.println("Engine Started....");
@@ -26,6 +28,12 @@ public class Car {
             System.out.println("Not started!");
     }
 
+    public void carInfo(){
+
+        //Here this keyword points to the current object
+        System.out.println(" Name = "+this.name + "\n Model = " + this.model 
+        + "\n year = " + this.year + "\n Engine = "+this.Engine); 
+    }
 
 
 }
