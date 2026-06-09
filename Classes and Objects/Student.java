c5dedbe27376223ed71b6d09394e40efcfc3dd99
Abrar Ahmed <unarabrar80@gmail.com>
2026-06-09 08:23:21 +0500

    Updated Student.java

--- Classes and Objects/Student.java
+++ Classes and Objects/Student.java
@@ -47,9 +47,57 @@ public class Student {
     public void displayInfo(){
         System.out.println();
         System.out.println("Student Information: ");
-        System.out.println(" Name: " + this.name + "\n StudentId: " + studentId + "\n GPA: " + gpa);
+        System.out.println(" Name: " + name + "\n StudentId: " + studentId + "\n GPA: " + gpa);
         
         }
+        
+        
+    public char calculateGrade(){
+        
+        if(gpa >= 3.7)
+         return 'A';
+      
+        else if(gpa >= 3.0)
+         return 'B';
+      
+       else if(gpa >= 2.0)
+         return 'C';
+      
+       else if(gpa >= 1.0)
+        return 'D';
+       
+       else
+        return 'F';
+        
+    }
+    
+    
+    public void getGradeLetter(){
+        
+        switch(calculateGrade()){
+        
+          case 'A':
+            System.out.println("Congratulations you have got A Grade");
+              break;
+              
+          case 'B':
+            System.out.println("Congratulations you have got B Grade");
+              break;
+              
+          case 'C':
+            System.out.println("You have got C Grade");
+              break;
+              
+          case 'D':
+            System.out.println("D Grade");
+              break;
+              
+          case 'F':
+            System.out.println("Unfortunately You are Fail");
+            
+        }
+            
+   }
         
         
         
