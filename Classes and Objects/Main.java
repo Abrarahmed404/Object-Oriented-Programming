cab5e43c7bb12ca5bd4b35edabdbac56639b71a3
Abrarahmed404 <unarabrar80@gmail.com>
2026-06-09 07:50:14 +0500

    created objects of Student class

--- Classes and Objects/Main.java
+++ Classes and Objects/Main.java
@@ -5,17 +5,17 @@ public class Main{
         Dog d1 = new Dog("Tommy", "Brown"); 
 
         //Barks methode is called
-        d1.barks();                                    
+            d1.barks();                                    
         
 
         //Car object using default constructor
         Car c1 = new Car();   
         //It has not any values given yet 
         
-        c1.name = "Toyota";
-        c1.model = "Unknown";
-        c1.year = 2024;
-        c1.Engine ="V8 Engine";
+            c1.name = "Toyota";
+            c1.model = "Unknown";
+            c1.year = 2024;
+            c1.Engine ="V8 Engine";
         //car object is complete
 
         //Car object using parameterized constructor
@@ -23,16 +23,35 @@ public class Main{
         //Here values are given when object is bieng created and constructor is called.
         
         
-        c2. carInfo();
-        System.out.println();
+            c2. carInfo();
+            System.out.println();
 
         
         Book b1 = new Book("Intersteller", "Erwin shrewdinger",22084, "7th");
         
         Book b2 = new Book("Lamed", "Abrar Ahmed",22076, "1st");
 
-        b1.displayBook();
-        System.out.println();
-        b2.displayBook();
+            b1.displayBook();
+            System.out.println();
+            b2.displayBook();
+
+
+         //objects of Student class
+        
+        //using default constructor
+      Student s1 = new Student();
+      
+           s1.setName("Abrar");
+           s1.setStudentId(282086);
+           s1.setGpa(3.69);
+      
+           s1.displayInfo();
+      
+      
+      //using parameterized constructor
+      Student s2 = new Student("Qadir", 282077, 3.75);
+        
+        
+           s2.displayInfo();
     }
 }
