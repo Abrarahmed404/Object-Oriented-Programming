5cc4217051ad9c2ab78a3c56d78e57846c548d3b
Abrar Ahmed <unarabrar80@gmail.com>
2026-06-11 21:57:28 +0500

    Implemented the concept of overloading

---
+++ Polymorphism/Calculator.java
@@ -0,0 +1,57 @@
+public class Calculator{
+
+	    private double result;
+	
+	    public Calculator(){
+	      System.out.println("Default constructor");
+	      result=0.0;
+	    }
+	
+	    public Calculator(double result){
+	      System.out.println("Parameterized constructor");
+	      this.result = result;
+	    }
+	    
+	    public void resetResult(){
+	     result=0.0;
+	    }
+	    
+	    public double getResult(){
+        
+        return result;
+      }
+	    
+	    
+	    //taking two int parameters
+	    public int add(int a,int b){
+	      System.out.println("method 1");
+	      result =result + (a+b);
+	      return (int)result;
+	    }
+	    
+	    
+	    //taking two parameters int and double
+	    public double add(int a, double b){
+	      System.out.println("method 2");
+	      result =result + (a+b);
+	     return result;
+	    }
+	    
+	    
+	    //taking two parameters int and double in different order
+	    public double add(double a, int b){
+	      System.out.println("method 3");
+	      result =result + (a+b);
+	      return result;
+	    }
+	    
+	    
+	    //taking three inputs
+	    public int add(int a, int b, int c){
+	      System.out.println("method 4");
+	      result =result + (a+b);
+	      return (int)result;
+	    }
+	    
+	    
+}	    
