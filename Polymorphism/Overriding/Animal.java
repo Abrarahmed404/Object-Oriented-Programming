db8d27af803703dc9a785242c0ad5bf68ba23563
Abrar Ahmed <unarabrar80@gmail.com>
2026-06-22 23:08:34 +0500

    implemented Animal class

---
+++ Polymorphism/Overriding/Animal.java
@@ -0,0 +1,21 @@
+public class Animal{
+    int legs;
+    boolean hasTail;
+    double hieght;
+   
+   
+   public void eat(){
+   System.out.print("eating");
+   }
+   
+   
+    Animal(int legs, double hieght, boolean hasTail){
+    
+    this.legs = legs;
+    this.hasTail = hasTail;
+    this.hieght = hieght;
+    
+    }
+    
+    
+}
