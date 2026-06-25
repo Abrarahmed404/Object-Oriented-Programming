1f0b27351dfca245b39455a67d35bc05bd7f2e15
Abrar Ahmed <unarabrar80@gmail.com>
2026-06-25 23:39:26 +0500

    Implement multi-level inheritance example in Main.java

--- Inheritance/MultiLevel Inheritance/Main.java
+++ Inheritance/MultiLevel Inheritance/Main.java
@@ -1,1 +1,17 @@
-
+public class Main{
+    public static void main(String[] args){
+    
+    //Grand parent class object
+    Person p1 = new Person("Kashif","Manthar",21,"Sindh,Shikarpur,Wazirabad");
+    p1.displayInfo();
+    
+    //Child of grand Parent class object
+    Employ e1 = new Employ("Abrar","Mujeeb",19, "sindh,district shikarpur,Taluka Lakhi, UC Wazirabad","unarabrar80@gmail.com",83000,22108);
+    e1.displayInfo();
+    
+    //Lowest child class object which has properties of both classes
+    Professor pf1 = new Professor("Fahad","Mushtaque",19,"sindh,district shikarpur,Taluka Lakhi, UC Wazirabad","fahd879@gmail.com",65000,22110,"Cyber Security",9);
+    pf1.displayInfo();
+    
+    }
+}
