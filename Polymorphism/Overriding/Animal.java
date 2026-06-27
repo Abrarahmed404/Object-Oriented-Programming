676d0ed07a25b2af5ae7614a02cd37db77099292
Abrar Ahmed <unarabrar80@gmail.com>
2026-06-27 15:49:46 +0500

    Updated Animal class
    Added a non-parameterized constructor to the Animal class.

--- Polymorphism/Overriding/Animal.java
+++ Polymorphism/Overriding/Animal.java
@@ -8,13 +8,22 @@ public class Animal{
    System.out.print("eating");
    }
    
-   
+
+
+    //Parametarized Constructor
     Animal(int legs, double hieght, boolean hasTail){
     
     this.legs = legs;
     this.hasTail = hasTail;
     this.hieght = hieght;
     
+    }
+
+
+
+    //Non parametarized Constructor
+    Animal(){
+
     }
     
     
