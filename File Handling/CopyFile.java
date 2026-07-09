9455db20945972a3b6084825771fefa3a50d5acf
Abrar Ahmed <unarabrar80@gmail.com>
2026-07-09 17:27:08 +0500

    Enhance comments in CopyFile.java for clarity
    Added comments to clarify the purpose of FileInputStream and FileOutputStream.

--- File Handling/CopyFile.java
+++ File Handling/CopyFile.java
@@ -2,10 +2,15 @@ import java.io.*;
 
   public class CopyFile{
     public static void main(String [] args) throws IOException{
+
+      //FileInputStream is predefined class used to read a File
          FileInputStream fis = new FileInputStream("/home/abrar-ahmed/Desktop/Qadir.txt"); 
+
+      //FileutputStream is predefined class used to create and Write in a File
          FileOutputStream fos = new FileOutputStream("/home/abrar-ahmed/Desktop/Abrar.txt");
-           int i;
+
       
+           int i;      
            while((i = fis.read())!=-1){   // here i will store the ASCI value of Char
         
             fos.write((char)i);    //Here ASCI Value of char will be changed to characte and will be written in second file 
